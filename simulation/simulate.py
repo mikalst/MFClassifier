713b3e362bd39d9942c59a549f9e49397810d626
Mikal Stapnes <mstapnes@gmail.com>
2019-10-17 09:06:01 +0200

    Simulation now almost completely different

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -2,7 +2,10 @@ import numpy as np
 import scipy.sparse
 
 
-def simulate_mask(X, f_of_t, expectation_of_priors, variances, level=0.3):
+def simulate_mask(
+        X,
+        parameters
+    ):
     """Simulation of a missing data mask.
     
     Parameters
@@ -17,25 +20,38 @@ def simulate_mask(X, f_of_t, expectation_of_priors, variances, level=0.3):
     ----------
     mask : The resulting simulated mask.
     """
+    mask_transition_expectations = parameters['mask_transition_expectations']
+    mask_transition_variances = parameters['mask_transition_variances']
+    mask_memory_length = parameters['memory_length']
+    mask_level = parameters['mask_level']
+
     mask = np.zeros_like(X, dtype=np.bool)
-    individual_probs = np.empty((X.shape[0], expectation_of_priors.shape[0]))
+    observed_values = np.zeros_like(X)
+    individual_probs = np.empty((X.shape[0], mask_transition_expectations.shape[0]))
 
     # Assign individual transition probabilities
-    for i in range(expectation_of_priors.shape[0]):
-        individual_probs[:, i] = np.random.beta(variances[i]*expectation_of_priors[i],
-                                                variances[i]*(1-expectation_of_priors[i]), size=(X.shape[0]))
-    
+    for i in range(mask_transition_expectations.shape[0]):
+        individual_probs[:, i] = np.random.beta(mask_transition_variances[i]*mask_transition_expectations[i],
+                                                mask_transition_variances[i]*(1-mask_transition_expectations[i]), size=(X.shape[0]))
+
     for t in range(X.shape[1] - 1):
-        values_at_current_time = X[:, t]*mask[:, t]
-        indices = ((np.arange(5))[:, None] == values_at_current_time).T
-        p = 1e3*level * individual_probs[indices] * f_of_t(t)
+        # Find last remembered values 
+        last_remembered_values = observed_values[np.arange(observed_values.shape[0]), t+1-np.argmax(observed_values[:, t+1:max(0, t-mask_memory_length):-1] != 0, axis=1)]
+
+        indices = ((np.arange(np.max(X)+1))[:, None] == last_remembered_values).T
+        p = mask_level*individual_probs[indices]
         r = np.random.uniform(size=(X.shape[0]))
         mask[r <= p, t+1] = True
+        observed_values[r <= p, t+1] = X[r <= p, t+1]
         
     return mask
 
 
-def simulate_ordinal_from_float(matrix, pdf, ordinal_domain, kernel_parameter=1.0, truncate_limits=[0.5, 1.0]):
+def simulate_ordinal_from_float(
+        basis, explained_variance_ratios,
+        parameters,
+        return_float=False
+    ):
     """Simulation of ordinal data from scores.
     
     Parameters
@@ -50,6 +66,18 @@ def simulate_ordinal_from_float(matrix, pdf, ordinal_domain, kernel_parameter=1.
     ----------
     res : The resulting matrix of ordinal data.
     """
+    original_data_pdf = parameters['original_data_pdf']
+    ordinal_domain = parameters['output_domain']
+    kernel_parameter = parameters['kernel_parameter']
+    truncate_limits = parameters['truncate_limits']
+
+    weights = np.random.uniform(size=(38001, basis.shape[0])) * explained_variance_ratios
+    # Squares should sum up to 1
+    weights_normalized = weights# / np.reshape(np.sqrt((weights**2).sum(axis=1)), (38001, 1))
+
+    # Generate 38001 rows as random linear combinations of these singular 5 components
+    matrix = weights_normalized@basis
+
     lower_truncate_limit, upper_truncate_limit = np.quantile(matrix, truncate_limits)
     domain_max = np.max(ordinal_domain)
     domain_min = np.min(ordinal_domain)
@@ -67,7 +95,7 @@ def simulate_ordinal_from_float(matrix, pdf, ordinal_domain, kernel_parameter=1.
 
     # Initialization
     column_repeated = np.repeat(matrix[:, 0], 4).reshape((matrix.shape[0], 4), order='C')
-    d = distribution(column_repeated, domain_repeated) * pdf
+    d = distribution(column_repeated, domain_repeated) * original_data_pdf
     cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1), (matrix.shape[0], 1)), axis=1)
 
     u = np.random.uniform(size=(matrix.shape[0], 1))
@@ -79,17 +107,25 @@ def simulate_ordinal_from_float(matrix, pdf, ordinal_domain, kernel_parameter=1.
         column_repeated = np.repeat(matrix[:, j], 4).reshape((matrix.shape[0], 4), order='C')
         last_int_column_repeated = np.repeat(res[:, j-1], 4).reshape((matrix.shape[0], 4), order='C')
         neighbours = is_neighbours(last_int_column_repeated, domain_repeated)
-        d = distribution(column_repeated, domain_repeated) * neighbours * pdf
+        d = distribution(column_repeated, domain_repeated) * neighbours * original_data_pdf
         cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1), (matrix.shape[0], 1)), axis=1)
 
         u = np.random.uniform(size=(matrix.shape[0], 1))
         indices = np.argmax(u <= cdf, axis=1)
         res[:, j] = ordinal_domain[indices]
-    
-    return res
+
+    if return_float:
+        return res, matrix
+    else:
+        return res
 
 
-def simulate_synthetic(parameters_simulate_ordinal, parameters_simulate_mask):
+def simulate_synthetic(
+        basis, explained_variance_ratios,
+        parameters_simulate_ordinal,
+        parameters_simulate_mask,
+        return_masked=False
+    ):
     """Simulation of a complete synthetic dataset.
     
     Parameters
@@ -101,29 +137,19 @@ def simulate_synthetic(parameters_simulate_ordinal, parameters_simulate_mask):
     ----------
     X_synthetic_final : Simulated dataset in CSR format.
     """
-    basis = parameters_simulate_ordinal['basis']
-    explained_variance_ratios = parameters_simulate_ordinal['explained_variance_ratios']
-    original_data_pdf = parameters_simulate_ordinal['original_data_pdf']
-    output_domain = parameters_simulate_ordinal['output_domain']
-    kernel_parameter = parameters_simulate_ordinal['kernel_parameter']
-    truncate_limits = parameters_simulate_ordinal['truncate_limits']
-    
-    marginal_distribution_time = parameters_simulate_mask['marginal_distribution_time']
-    mask_transition_expectations = parameters_simulate_mask['mask_transition_expectations']
-    mask_transition_variances = parameters_simulate_mask['mask_transition_variances']
-    mask_level = parameters_simulate_mask['mask_level']
+    X_synthetic_ordinal = simulate_ordinal_from_float(
+        basis, explained_variance_ratios,
+        parameters=parameters_simulate_ordinal
+    )
 
-    weights = np.random.uniform(size=(38001, basis.shape[0])) * explained_variance_ratios
-    weights_normalized = weights
-
-    X_synthetic_float = weights_normalized@basis
-
-    X_synthetic_ordinal = simulate_ordinal_from_float(X_synthetic_float, original_data_pdf, ordinal_domain = output_domain,
-                                                kernel_parameter=2.5, truncate_limits=[0.75, .9975])
-
-    mask = simulate_mask(X_synthetic_ordinal, marginal_distribution_time, mask_transition_expectations,
-                        mask_transition_variances, level=mask_level)
+    mask = simulate_mask(
+        X_synthetic_ordinal,
+        parameters=parameters_simulate_mask
+    )
 
     X_synthetic_final = scipy.sparse.csr_matrix(X_synthetic_ordinal*mask)
 
-    return X_synthetic_final
+    if return_masked:
+        return X_synthetic_final, X_synthetic_ordinal
+    else:
+        return X_synthetic_final

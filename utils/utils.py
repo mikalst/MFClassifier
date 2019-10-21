3fb935e7e40eaa8301640af03d0ec623fa4a36d9
Mikal Stapnes <mstapnes@gmail.com>
2019-10-21 17:45:35 +0200

    Change smoothing factor

--- utils/utils.py
+++ utils/utils.py
@@ -12,8 +12,7 @@ def extract_smoothened_arch_timelines(pca_components):
 
     for i, tl in enumerate(pca_components):
         spl = scipy.interpolate.UnivariateSpline(np.linspace(0, 320, 321), tl, k=3)
-        spl.set_smoothing_factor(1e-3)
+        spl.set_smoothing_factor(5e-1)
         pca_components_smooth[i] = spl(np.linspace(0, 320, 321))
 
     return pca_components_smooth
-    

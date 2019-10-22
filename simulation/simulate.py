55ef8cbffac5df66fd6132f5e9deeb061d3630ec
Mikal Stapnes <mstapnes@gmail.com>
2019-10-22 14:52:47 +0200

    Linting and simulate_synthetic can now return synthetic float matrix

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -3,11 +3,11 @@ import scipy.sparse
 
 
 def simulate_mask(
-        X,
-        parameters
-    ):
+    X,
+    parameters
+):
     """Simulation of a missing data mask.
-    
+
     Parameters
     ----------
     X : The unmasked data.
@@ -15,7 +15,7 @@ def simulate_mask(
     expectation_of_priors : ...
     variances : Expectation and variance used in the priors of the corresponding ordinal value
     level : Parameter for tuning the global length of the mask. 
-    
+
     Returns
     ----------
     mask : The resulting simulated mask.
@@ -27,7 +27,8 @@ def simulate_mask(
 
     mask = np.zeros_like(X, dtype=np.bool)
     observed_values = np.zeros_like(X)
-    individual_probs = np.empty((X.shape[0], mask_transition_expectations.shape[0]))
+    individual_probs = np.empty(
+        (X.shape[0], mask_transition_expectations.shape[0]))
 
     # Assign individual transition probabilities
     for i in range(mask_transition_expectations.shape[0]):
@@ -35,25 +36,27 @@ def simulate_mask(
                                                 mask_transition_variances[i]*(1-mask_transition_expectations[i]), size=(X.shape[0]))
 
     for t in range(X.shape[1] - 1):
-        # Find last remembered values 
-        last_remembered_values = observed_values[np.arange(observed_values.shape[0]), t+1-np.argmax(observed_values[:, t+1:max(0, t-mask_memory_length):-1] != 0, axis=1)]
+        # Find last remembered values
+        last_remembered_values = observed_values[np.arange(observed_values.shape[0]), t+1-np.argmax(
+            observed_values[:, t+1:max(0, t-mask_memory_length):-1] != 0, axis=1)]
 
-        indices = ((np.arange(np.max(X)+1))[:, None] == last_remembered_values).T
+        indices = ((np.arange(np.max(X)+1))
+                   [:, None] == last_remembered_values).T
         p = mask_level*individual_probs[indices]
         r = np.random.uniform(size=(X.shape[0]))
         mask[r <= p, t+1] = True
         observed_values[r <= p, t+1] = X[r <= p, t+1]
-        
+
     return mask
 
 
 def simulate_ordinal_from_float(
-        basis, explained_variance_ratios,
-        parameters,
-        return_float=False
-    ):
+    basis, explained_variance_ratios,
+    parameters,
+    return_float=False
+):
     """Simulation of ordinal data from scores.
-    
+
     Parameters
     ----------
     matrix : Scores.
@@ -61,7 +64,7 @@ def simulate_ordinal_from_float(
     ordinal_domain : Possible values of the output.
     kernel_parameter : The strength with which a score will seek towards the corresponding ordinal value.
     truncate_limits : Limits for tuning the tails of the resulting distribution.
-    
+
     Returns
     ----------
     res : The resulting matrix of ordinal data.
@@ -71,32 +74,39 @@ def simulate_ordinal_from_float(
     kernel_parameter = parameters['kernel_parameter']
     truncate_limits = parameters['truncate_limits']
 
-    weights = np.random.uniform(size=(38001, basis.shape[0])) * explained_variance_ratios
+    weights = np.random.uniform(
+        size=(38001, basis.shape[0])) * explained_variance_ratios
     # Squares should sum up to 1
-    weights_normalized = weights# / np.reshape(np.sqrt((weights**2).sum(axis=1)), (38001, 1))
+    # / np.reshape(np.sqrt((weights**2).sum(axis=1)), (38001, 1))
+    weights_normalized = weights
 
     # Generate 38001 rows as random linear combinations of these singular 5 components
     matrix = weights_normalized@basis
 
-    lower_truncate_limit, upper_truncate_limit = np.quantile(matrix, truncate_limits)
+    lower_truncate_limit, upper_truncate_limit = np.quantile(
+        matrix, truncate_limits)
     domain_max = np.max(ordinal_domain)
     domain_min = np.min(ordinal_domain)
 
-    matrix = domain_min + (domain_max - domain_min) * (matrix - lower_truncate_limit)/(upper_truncate_limit - lower_truncate_limit)
+    matrix = domain_min + (domain_max - domain_min) * (matrix -
+                                                       lower_truncate_limit)/(upper_truncate_limit - lower_truncate_limit)
     matrix[matrix < domain_min] = domain_min
     matrix[matrix > domain_max] = domain_max
 
-    distribution = lambda x, dom: np.exp(-kernel_parameter*np.abs(x - dom))
-    is_neighbours = lambda x, y: np.abs(x - y) <= 1
+    def distribution(x, dom): return np.exp(-kernel_parameter*np.abs(x - dom))
+    def is_neighbours(x, y): return np.abs(x - y) <= 1
 
-    domain_repeated = np.repeat(ordinal_domain, matrix.shape[0]).reshape((matrix.shape[0], ordinal_domain.shape[0]), order='F')
+    domain_repeated = np.repeat(ordinal_domain, matrix.shape[0]).reshape(
+        (matrix.shape[0], ordinal_domain.shape[0]), order='F')
 
     res = np.empty_like(matrix)
 
     # Initialization
-    column_repeated = np.repeat(matrix[:, 0], 4).reshape((matrix.shape[0], 4), order='C')
+    column_repeated = np.repeat(matrix[:, 0], 4).reshape(
+        (matrix.shape[0], 4), order='C')
     d = distribution(column_repeated, domain_repeated) * original_data_pdf
-    cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1), (matrix.shape[0], 1)), axis=1)
+    cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1),
+                                   (matrix.shape[0], 1)), axis=1)
 
     u = np.random.uniform(size=(matrix.shape[0], 1))
     indices = np.argmax(u <= cdf, axis=1)
@@ -104,11 +114,15 @@ def simulate_ordinal_from_float(
 
     # Timestepping
     for j in range(1, matrix.shape[1]):
-        column_repeated = np.repeat(matrix[:, j], 4).reshape((matrix.shape[0], 4), order='C')
-        last_int_column_repeated = np.repeat(res[:, j-1], 4).reshape((matrix.shape[0], 4), order='C')
+        column_repeated = np.repeat(matrix[:, j], 4).reshape(
+            (matrix.shape[0], 4), order='C')
+        last_int_column_repeated = np.repeat(
+            res[:, j-1], 4).reshape((matrix.shape[0], 4), order='C')
         neighbours = is_neighbours(last_int_column_repeated, domain_repeated)
-        d = distribution(column_repeated, domain_repeated) * neighbours * original_data_pdf
-        cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1), (matrix.shape[0], 1)), axis=1)
+        d = distribution(column_repeated, domain_repeated) * \
+            neighbours * original_data_pdf
+        cdf = np.cumsum(d / np.reshape(np.sum(d, axis=1),
+                                       (matrix.shape[0], 1)), axis=1)
 
         u = np.random.uniform(size=(matrix.shape[0], 1))
         indices = np.argmax(u <= cdf, axis=1)
@@ -121,13 +135,14 @@ def simulate_ordinal_from_float(
 
 
 def simulate_synthetic(
-        basis, explained_variance_ratios,
-        parameters_simulate_ordinal,
-        parameters_simulate_mask,
-        return_masked=False
-    ):
+    basis, explained_variance_ratios,
+    parameters_simulate_ordinal,
+    parameters_simulate_mask,
+    return_masked=False,
+    return_float=False,
+):
     """Simulation of a complete synthetic dataset.
-    
+
     Parameters
     ----------
     parameters_simulate_ordinal : See simulate_ordinal_from_float.
@@ -137,10 +152,17 @@ def simulate_synthetic(
     ----------
     X_synthetic_final : Simulated dataset in CSR format.
     """
-    X_synthetic_ordinal = simulate_ordinal_from_float(
-        basis, explained_variance_ratios,
-        parameters=parameters_simulate_ordinal
-    )
+    if return_float:
+        X_synthetic_ordinal, X_synthethic_float = simulate_ordinal_from_float(
+            basis, explained_variance_ratios,
+            parameters=parameters_simulate_ordinal,
+            return_float=True
+        )
+    else:
+        X_synthetic_ordinal = simulate_ordinal_from_float(
+            basis, explained_variance_ratios,
+            parameters=parameters_simulate_ordinal
+        )
 
     mask = simulate_mask(
         X_synthetic_ordinal,
@@ -149,7 +171,9 @@ def simulate_synthetic(
 
     X_synthetic_final = scipy.sparse.csr_matrix(X_synthetic_ordinal*mask)
 
-    if return_masked:
+    if return_masked & return_float:
+        return X_synthetic_final, X_synthetic_ordinal, X_synthethic_float
+    elif return_masked:
         return X_synthetic_final, X_synthetic_ordinal
     else:
         return X_synthetic_final

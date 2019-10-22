2fc9ce9b01d7ca1df99966d99c9fb184e27d0e19
Mikal Stapnes <mstapnes@gmail.com>
2019-10-22 14:31:51 +0200

    Allow plotting only large singular vectors. Also lint

--- utils/plotting.py
+++ utils/plotting.py
@@ -14,58 +14,58 @@ def plot_distribution_per_woman(matrix1, matrix2, sparse=False, title_postfix=""
         data1 = np.sum(matrix1 == 3, axis=1).A1
         data3 = np.sum(matrix1 == 4, axis=1).A1
         data5 = np.sum(matrix1 != 0, axis=1).A1
-        
+
     else:
         data1 = np.sum(matrix1 == 3, axis=1)
         data3 = np.sum(matrix1 == 4, axis=1)
         data5 = np.sum(matrix1 != 0, axis=1)
-        
+
     data2 = np.sum(matrix2 == 3, axis=1).A1
     data4 = np.sum(matrix2 == 4, axis=1).A1
     data6 = np.sum(matrix2 != 0, axis=1).A1
-    
+
     plt.subplot(3, 2, 1)
     plt.title("Synthetic" + title_postfix)
     plt.ylabel("All risk levels")
-    plt.hist(data5, bins = np.linspace(0, 40).astype(np.int), edgecolor='k')
-    
+    plt.hist(data5, bins=np.linspace(0, 40).astype(np.int), edgecolor='k')
+
     plt.subplot(3, 2, 2)
     plt.title("Original" + title_postfix)
     plt.ylabel("All risk levels")
-    plt.hist(data6, bins = np.linspace(0, 40).astype(np.int), edgecolor='k')
-    
+    plt.hist(data6, bins=np.linspace(0, 40).astype(np.int), edgecolor='k')
+
     plt.subplot(3, 2, 3)
     plt.ylabel("risk level 3")
-    plt.hist(data1, bins = np.linspace(0, 20).astype(np.int), edgecolor='k')
+    plt.hist(data1, bins=np.linspace(0, 20).astype(np.int), edgecolor='k')
 
     plt.subplot(3, 2, 4)
-    plt.hist(data2, bins = np.linspace(0, 20).astype(np.int), edgecolor='k')
-    
+    plt.hist(data2, bins=np.linspace(0, 20).astype(np.int), edgecolor='k')
+
     plt.subplot(3, 2, 5)
     plt.ylabel("risk level 4")
-    plt.hist(data3, bins = np.linspace(0, 4).astype(np.int), edgecolor='k')
+    plt.hist(data3, bins=np.linspace(0, 4).astype(np.int), edgecolor='k')
 
     plt.subplot(3, 2, 6)
-    plt.hist(data4, bins = np.linspace(0, 4).astype(np.int), edgecolor='k')
+    plt.hist(data4, bins=np.linspace(0, 4).astype(np.int), edgecolor='k')
     plt.show()
-    
-    
+
+
 def plot_distribution_per_age(matrix1, matrix2, sparse=False, title_postfix=""):
     fig = plt.figure(figsize=(12, 10))
 
     data1, data3, data5 = distribution_per_age(matrix1)
     data2, data4, data6 = distribution_per_age(matrix2)
-    
+
     plt.subplot(3, 2, 1)
     plt.title("Synthetic" + title_postfix)
     plt.ylabel("All risk levels")
     plt.scatter(np.linspace(0, 320, 321), data5, marker="x")
-    
+
     plt.subplot(3, 2, 2)
     plt.title("Original" + title_postfix)
     plt.ylabel("All risk levels")
     plt.scatter(np.linspace(0, 320, 321), data6, marker="x")
-    
+
     plt.subplot(3, 2, 3)
     plt.title("Synthetic" + title_postfix)
     plt.ylabel("risk level 3")
@@ -74,7 +74,7 @@ def plot_distribution_per_age(matrix1, matrix2, sparse=False, title_postfix=""):
     plt.subplot(3, 2, 4)
     plt.title("Original")
     plt.scatter(np.linspace(0, 320, 321), data2, marker="x")
-    
+
     plt.subplot(3, 2, 5)
     plt.ylabel("risk level 4")
     plt.scatter(np.linspace(0, 320, 321), data3, marker="x")
@@ -95,7 +95,7 @@ def plot_sparsity(matrix1, matrix2):
     plt.title("Original")
     out = plt.spy(matrix2, aspect="auto", markersize=0.06)
     plt.show()
-    
+
 
 def plot_visual(matrix1, matrix2, titles=("Synthetic", "Original")):
     fig = plt.figure(figsize=(14, 6))
@@ -120,44 +120,51 @@ def plot_visual(matrix1, matrix2, titles=("Synthetic", "Original")):
 
 def plot_compare_svd_with_basis(S, path_to_basis):
     original_singular_components = np.load(path_to_basis)
-    n_components = original_singular_components.shape[0]        
+    n_components = original_singular_components.shape[0]
 
     pca = sklearn.decomposition.PCA(n_components=n_components, copy=True, whiten=True,
-        svd_solver='auto', tol=0.0, iterated_power='auto',
-        random_state=None)
+                                    svd_solver='auto', tol=0.0, iterated_power='auto',
+                                    random_state=None)
     pca.fit(S)
 
     plt.figure(figsize=(12, 4))
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.title("Recovered")
     for sc in pca.components_[:n_components]:
         plt.plot(sc)
-        
-    plt.subplot(1,2,2)
+
+    plt.subplot(1, 2, 2)
     plt.title("Original")
     for sc in original_singular_components:
         plt.plot(sc)
     plt.show()
 
-    
-def plot_compare_svd_with_svd(X1, X2, n_components):
-    pca1 = sklearn.decomposition.PCA(n_components=n_components, copy=True, whiten=True,
-        svd_solver='auto', tol=0.0, iterated_power='auto',
-        random_state=None)
+
+def plot_compare_svd_with_svd(X1, X2, components):
+    try:
+        first_comp = components[0]
+        last_comp = components[1]
+    except TypeError:
+        first_comp = 0
+        last_comp = components
+
+    pca1 = sklearn.decomposition.PCA(n_components=last_comp, copy=True, whiten=True,
+                                     svd_solver='auto', tol=0.0, iterated_power='auto',
+                                     random_state=None)
     pca1.fit(X1)
-    pca2 = sklearn.decomposition.PCA(n_components=n_components, copy=True, whiten=True,
-        svd_solver='auto', tol=0.0, iterated_power='auto',
-        random_state=None)
+    pca2 = sklearn.decomposition.PCA(n_components=last_comp, copy=True, whiten=True,
+                                     svd_solver='auto', tol=0.0, iterated_power='auto',
+                                     random_state=None)
     pca2.fit(X2)
 
     plt.figure(figsize=(12, 4))
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.title("Recovered")
-    for sc in pca1.components_[:n_components]:
+    for sc in pca1.components_[first_comp:last_comp]:
         plt.plot(sc)
-        
-    plt.subplot(1,2,2)
+
+    plt.subplot(1, 2, 2)
     plt.title("Original")
-    for sc in pca2.components_[:n_components]:
+    for sc in pca2.components_[first_comp:last_comp]:
         plt.plot(sc)
     plt.show()

0a896b5f0e9edaeb7ce8e09e3c51727683e50175
Mikal Stapnes <mstapnes@gmail.com>
2019-11-12 11:43:10 +0100

    Logical error

--- utils/stats.py
+++ utils/stats.py
@@ -8,7 +8,7 @@ def compute_discrete_distribution_from_sparse_matrix(matrix):
     else:
         x, px = np.unique(matrix.flat, return_counts=True)
     
-    px_nonzero = px[x != 0] / np.sum(px[x != 0])
+    px_nonzero = px[x != x[0]] / np.sum(px[x != x[0]])
     
     return (x[x != 0], px_nonzero, np.cumsum(px_nonzero))
 

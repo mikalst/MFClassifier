8538029de617525cf13fbc4ae0fbd904723e2a0b
Mikal Stapnes <mstapnes@gmail.com>
2019-11-19 10:03:01 +0100

    Clean code

--- ridge/penalty.py
+++ ridge/penalty.py
@@ -10,66 +10,14 @@ class RidgePenalty:
         self.lambda2 = kwargs['lambda2']
         self.lambda3 = kwargs['lambda3']
 
-        self.predict_method = kwargs['predict_method']
-
         self.X = kwargs['X']
-        self.Xtrain = np.copy(self.X)
-
-        if kwargs['predict_method'] == 'naive':
-            self.predict_entries = self.select_random_entries(percentage=0.10)
-            self.Xtrain[self.predict_entries] = 0.0
-
-        elif kwargs['predict_method'] == 'realistic':
-            self.predict_window = kwargs['predict_window']
-            predict_rows, predict_cols = [], []
-
-            row_candidates = np.random.choice(
-                np.arange(self.X.shape[0]), size=int(0.10*self.X.shape[0]), replace=False)
-            is_nonzero = self.Xtrain[row_candidates] != 0
-            counts_nonzero = np.cumsum(is_nonzero, axis=1)
-
-            # Choose rth nonzero entry in row randomly
-            rth_nonzero_predict = np.random.randint(
-                5*np.ones(len(row_candidates)),
-                np.amax((3*counts_nonzero[:, -1]//4, 6 *
-                         np.ones(len(row_candidates))), axis=0)
-            )
-            # Find column index of rth nonzero entry in row
-            col_candidates = np.argmax(
-                counts_nonzero == rth_nonzero_predict[:, None], axis=1)
-
-            valid_row_candidates = row_candidates[col_candidates > 0]
-            valid_col_candidates = col_candidates[col_candidates > 0]
-
-            for i, row in enumerate(valid_row_candidates):
-                # Check if sufficient amount of nonzero entries
-                row_is_nonzero = self.Xtrain[row, :valid_col_candidates[i] -
-                                             self.predict_window] != 0
-                row_counts_nonzero = np.sum(row_is_nonzero)
-                if (row_counts_nonzero) > 3:
-                    self.Xtrain[row, valid_col_candidates[i] -
-                                self.predict_window:] = 0.0
-                    predict_rows.append(row)
-                    predict_cols.append(valid_col_candidates[i])
-
-            self.predict_rows = np.array(predict_rows)
-            self.predict_cols = np.array(predict_cols)
-
-        elif kwargs['predict_method'] == 'no_prediction':
-            pass
-
-        print(
-            'Assigning {:.2e} / {:.2e} observations to training'.format(
-                np.sum(self.Xtrain != 0), np.sum(self.X != 0))
-        )
-
         self.R = kwargs['R']
         self.J = kwargs['J']
         self.kappa = kwargs['kappa']
 
         # Set nonzero rows and columns automatically after assigning predict X
-        self.nonzero_rows_Xtrain, self.nonzero_cols_Xtrain = np.nonzero(
-            self.Xtrain)
+        self.nonzero_rows, self.nonzero_cols = np.nonzero(
+            self.X)
 
         self.N = self.X.shape[0]
         self.T = self.X.shape[1]
@@ -79,7 +27,7 @@ class RidgePenalty:
                                             np.random.uniform(size=self.T*self.k))).reshape((self.T, self.k))
         self.U = (np.ones(self.N*self.k) + (1 -
                                             np.random.uniform(size=self.N*self.k))).reshape((self.N, self.k))
-        self.S = self.Xtrain.copy()
+        self.S = self.X.copy()
 
         self.oldU = np.zeros_like(self.U)
         self.oldV = np.zeros_like(self.V)
@@ -89,7 +37,7 @@ class RidgePenalty:
         self.tol = kwargs['convergence_tol']
 
     def f(self, U, V):
-        return self.lambda0 * np.sum(np.power((self.Xtrain - U@V.T)[self.Xtrain != 0], 2)) + \
+        return self.lambda0 * np.sum(np.power((self.X - U@V.T)[self.X != 0], 2)) + \
             self.lambda1*np.linalg.norm(U)**2 + \
             self.lambda2*np.linalg.norm(V - self.J)**2 + \
             self.lambda3*np.linalg.norm(self.kappa@self.R@V)**2
@@ -147,8 +95,8 @@ class RidgePenalty:
 
     def solve3(self):
         S = self.U@self.V.T
-        S[self.nonzero_rows_Xtrain,
-            self.nonzero_cols_Xtrain] = self.Xtrain[self.nonzero_rows_Xtrain, self.nonzero_cols_Xtrain]
+        S[self.nonzero_rows,
+            self.nonzero_cols] = self.X[self.nonzero_rows, self.nonzero_cols]
 
         return S
 
@@ -176,104 +124,23 @@ class RidgePenalty:
         self.iteration += 1
         return False
 
-    def select_random_entries(self, percentage):
-        nonzero_rows, nonzero_cols = np.nonzero(self.X)
-        random_indices = np.random.choice(
-            np.arange(len(nonzero_rows)),
-            size=int(percentage*len(nonzero_rows))
-        )
-        return nonzero_rows[random_indices], nonzero_cols[random_indices]
+    def calculate_likelihood(self, X_predict, posterior=None):
 
-    def confusion_matrix(self, offset=0):
-        if self.predict_method == 'naive':
-            true = self.X[self.predict_entries]
-            pred = np.round((self.U@self.V.T)[self.predict_entries] + offset)
-            pred[pred < 1] = 1
-            pred[pred > 4] = 4
+        if posterior is None:
+            theta = 2.0
+            posterior = lambda d: np.exp(-theta * np.abs(d))
 
-            return sklearn.metrics.confusion_matrix(true, pred)
+        priors = np.sum(np.abs(self.U), axis=0) / np.sum(np.abs(self.U))
 
-        elif self.predict_method == 'realistic':
-            true = self.X[self.predict_rows, self.predict_cols]
-            pred = np.round((self.U@self.V.T)[
-                            self.predict_rows, self.predict_cols] + offset)
-            pred[pred < 1] = 1
-            pred[pred > 4] = 4
+        L = np.ones((X_predict.shape[0], self.k))
 
-            return sklearn.metrics.confusion_matrix(true, pred)
+        for i in range(X_predict.shape[0]):
+            row_nonzero_cols = X_predict[i] != 0
+            inside = (X_predict[i, row_nonzero_cols])[:,None] - self.V[row_nonzero_cols, :]
+            likelihood = np.prod(posterior(inside), axis=0)*priors
+            L[i] = likelihood
 
-    def confusion_matrix_forward_fill(self):
-        try:
-            assert(self.predict_method == 'realistic')
+        return L
 
-            true = self.X[self.predict_rows, self.predict_cols]
-            last_known = np.empty_like(true)
-            for index, i in enumerate(self.predict_rows):
-                j = self.Xtrain.shape[1] - 1 - \
-                    np.argmax(self.Xtrain[i, ::-1] != 0)
-                last_known[index] = self.Xtrain[i, j]
-            return sklearn.metrics.confusion_matrix(true, last_known)
-
-        except AssertionError:
-            print('prediction method should be set to realistic')
-
-
-def plot_ridge(ridge):
-    fig = plt.figure(figsize=(12, 12))
-
-    # Plot U, V, S and X
-    plt.subplot2grid((2, 6), (0, 0), colspan=2)
-    plt.title("$U$")
-    plt.imshow(ridge.U, aspect='auto')
-    plt.colorbar(orientation='horizontal', fraction=0.10, pad=0.10)
-
-    plt.subplot2grid((2, 6), (0, 2), colspan=2)
-    plt.title("$V$")
-    plt.imshow(ridge.V, aspect='auto')
-    plt.colorbar(orientation='horizontal', fraction=0.10, pad=0.10)
-
-    plt.subplot2grid((2, 6), (0, 4), colspan=2)
-    plt.title("$V$")
-    for j in range(ridge.V.shape[1]):
-        plt.plot(ridge.V[:, j])
-
-    ax1 = plt.subplot2grid((2, 6), (1, 0), colspan=3)
-    plt.title("$S$")
-    plt.imshow(ridge.S, aspect='auto')
-    plt.colorbar(orientation='horizontal', fraction=0.10, pad=0.10)
-
-    plt.subplot2grid((2, 6), (1, 3), colspan=3, sharey=ax1)
-    plt.title("$X$")
-    plt.imshow(ridge.X, aspect='auto')
-    plt.colorbar(orientation='horizontal', fraction=0.10, pad=0.10)
-
-    fig.tight_layout()
-    plt.show()
-
-    np.random.seed(12)
-    # Plot predictions for women with cancer
-    fig = plt.figure(figsize=(12, 20))
-
-    X_approx = ridge.U@ridge.V.T
-    X_approx_int = np.round(X_approx)
-    indices_with_risk = np.argwhere(np.any(ridge.X > 3, axis=1)).flatten()
-    indices = np.random.choice(indices_with_risk, size=32, replace=False)
-
-    for number, i in enumerate(indices):
-        plt.subplot(8, 4, number+1)
-        plt.plot(X_approx[i, :])
-        plt.plot(X_approx_int[i, :], linestyle='--')
-
-        # Plot predicted values in training set
-        x_i_train = (ridge.Xtrain[i, :])
-        plt.scatter(np.argwhere(x_i_train != 0),
-                    x_i_train[x_i_train != 0], color='k', marker='x')
-
-        # Plot predicted values not in training set
-        x_i = (ridge.X[i, :])
-        is_predicted = (x_i - x_i_train > 0)
-        plt.scatter(np.argwhere(is_predicted),
-                    x_i[is_predicted], color='g', marker='x')
-
-    fig.tight_layout()
-    plt.show()
+    def predict(self, X_predict):
+        pass

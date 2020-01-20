24a5611a7375f8b1d1489d6fe691b3487df6a53b
Mikal Stapnes <mstapnes@gmail.com>
2020-01-20 17:47:57 +0100

    Change variable names and functions names to correspond with project report

--- ridge/penalty.py
+++ ridge/penalty.py
@@ -3,100 +3,122 @@ import matplotlib.pyplot as plt
 import sklearn.metrics
 
 
-class RidgePenalty:
-    def __init__(self, kwargs):
-        self.lambda0 = kwargs['lambda0']
-        self.lambda1 = kwargs['lambda1']
-        self.lambda2 = kwargs['lambda2']
-        self.lambda3 = kwargs['lambda3']
+class MatrixFactorization:
+    """A class for the reconstruction of the underlying real-valued matrix M from an
+    observed integer-valued matrix Y, where Y ~ M is observed only at the nonzero entries.
+    """
 
-        self.X = kwargs['X']
-        self.R = kwargs['R']
-        self.J = kwargs['J']
-        self.kappa = kwargs['kappa']
+    def __init__(self, kwargs):
+        # Regularization parameters
+        self.lambda0 = kwargs["lambda0"]
+        self.lambda1 = kwargs["lambda1"]
+        self.lambda2 = kwargs["lambda2"]
+        self.lambda3 = kwargs["lambda3"]
+
+        self.X = kwargs["X"]
+        self.R = kwargs["R"]
+        self.J = kwargs["J"]
+        self.kappa = kwargs["kappa"]
 
         # Set nonzero rows and columns automatically after assigning predict X
-        self.nonzero_rows, self.nonzero_cols = np.nonzero(
-            self.X)
+        self.nonzero_rows, self.nonzero_cols = np.nonzero(self.X)
 
         self.N = self.X.shape[0]
         self.T = self.X.shape[1]
-        self.k = kwargs['k']
+        self.k = kwargs["k"]
 
-        self.V = (np.ones(self.T*self.k) + (1 -
-                                            np.random.uniform(size=self.T*self.k))).reshape((self.T, self.k))
-        self.U = (np.ones(self.N*self.k) + (1 -
-                                            np.random.uniform(size=self.N*self.k))).reshape((self.N, self.k))
+        # Initialize U, V and S
+        self.U = (
+            np.ones(self.N * self.k) + (1 - np.random.uniform(size=self.N * self.k))
+        ).reshape((self.N, self.k))
+        self.V = (
+            np.ones(self.T * self.k) + (1 - np.random.uniform(size=self.T * self.k))
+        ).reshape((self.T, self.k))
         self.S = self.X.copy()
 
         self.oldU = np.zeros_like(self.U)
         self.oldV = np.zeros_like(self.V)
 
         self.iteration = 0
-        self.total_iterations = kwargs['total_iterations']
-        self.tol = kwargs['convergence_tol']
+        self.total_iterations = kwargs["total_iterations"]
+        self.tol = kwargs["convergence_tol"]
 
     def f(self, U, V):
-        return self.lambda0 * np.sum(np.power((self.X - U@V.T)[self.X != 0], 2)) + \
-            self.lambda1*np.linalg.norm(U)**2 + \
-            self.lambda2*np.linalg.norm(V - self.J)**2 + \
-            self.lambda3*np.linalg.norm(self.kappa@self.R@V)**2
+        return (
+            self.lambda0 * np.sum(np.power((self.X - U @ V.T)[self.X != 0], 2))
+            + self.lambda1 * np.linalg.norm(U) ** 2
+            + self.lambda2 * np.linalg.norm(V - self.J) ** 2
+            + self.lambda3 * np.linalg.norm(self.kappa @ self.R @ V) ** 2
+        )
 
     def f1(self, u):
         U = u.reshape((self.N, self.k))
-        return self.lambda0*np.linalg.norm(self.S - U@self.V.T)**2 + self.lambda1*np.linalg.norm(U)**2
+        return (
+            self.lambda0 * np.linalg.norm(self.S - U @ self.V.T) ** 2
+            + self.lambda1 * np.linalg.norm(U) ** 2
+        )
 
-    def g1(self, u):
+    def grad1(self, u):
         U = u.reshape((self.N, self.k))
-        return (-2*self.lambda0*(self.S - U@self.V.T)@self.V + 2*self.lambda1*U).reshape(self.N*self.k)
+        return (
+            -2 * self.lambda0 * (self.S - U @ self.V.T) @ self.V + 2 * self.lambda1 * U
+        ).reshape(self.N * self.k)
 
     def solve1(self):
-        U = (np.linalg.solve(self.V.T@self.V+(self.lambda1/self.lambda0)
-                             * np.identity(self.k), self.V.T@self.S.T)).T
+        U = (
+            np.linalg.solve(
+                self.V.T @ self.V + (self.lambda1 / self.lambda0) * np.identity(self.k),
+                self.V.T @ self.S.T,
+            )
+        ).T
 
         return U
 
     def f2(self, v):
         V = v.reshape((self.T, self.k))
 
-        res1 = self.lambda0*np.linalg.norm(self.S - self.U@V.T)**2
-        res2 = self.lambda2*np.linalg.norm(V - self.J)**2
-        res3 = self.lambda3*np.linalg.norm((self.kappa@self.R)@V)**2
+        res1 = self.lambda0 * np.linalg.norm(self.S - self.U @ V.T) ** 2
+        res2 = self.lambda2 * np.linalg.norm(V - self.J) ** 2
+        res3 = self.lambda3 * np.linalg.norm((self.kappa @ self.R) @ V) ** 2
 
         return res1 + res2 + res3
 
-    def g2(self, v):
+    def grad2(self, v):
         V = v.reshape((self.T, self.k))
 
-        res1 = -2*self.lambda0*(self.S.T - V@self.U.T)@self.U
-        res2 = 2*self.lambda2*(V - self.J)
+        res1 = -2 * self.lambda0 * (self.S.T - V @ self.U.T) @ self.U
+        res2 = 2 * self.lambda2 * (V - self.J)
 
-        kappaR = self.kappa@self.R
-        res3 = 2*self.lambda3*(kappaR).T@((kappaR)@V)
+        kappaR = self.kappa @ self.R
+        res3 = 2 * self.lambda3 * (kappaR).T @ ((kappaR) @ V)
 
-        return (res1 + res2 + res3).reshape(self.T*self.k)
+        return (res1 + res2 + res3).reshape(self.T * self.k)
 
     def solve2(self):
         L1, Q1 = np.linalg.eigh(
-            self.U.T@self.U+(self.lambda2/self.lambda0)*np.identity(self.k))
+            self.U.T @ self.U + (self.lambda2 / self.lambda0) * np.identity(self.k)
+        )
 
-        kappaR = self.kappa@self.R
+        kappaR = self.kappa @ self.R
 
-        L2, Q2 = np.linalg.eigh(
-            (self.lambda3/self.lambda0)*(kappaR).T@(kappaR))
+        L2, Q2 = np.linalg.eigh((self.lambda3 / self.lambda0) * (kappaR).T @ (kappaR))
 
         # For efficiency purposes, these need to be evaluated in order
-        hatV = (Q2.T@(self.S.T@self.U+(self.lambda2/self.lambda0)
-                      * self.J))@Q1 / np.add.outer(L2, L1)
+        hatV = (
+            (Q2.T @ (self.S.T @ self.U + (self.lambda2 / self.lambda0) * self.J))
+            @ Q1
+            / np.add.outer(L2, L1)
+        )
 
-        V = Q2@(hatV@Q1.T)
+        V = Q2 @ (hatV @ Q1.T)
 
         return V
 
     def solve3(self):
-        S = self.U@self.V.T
-        S[self.nonzero_rows,
-            self.nonzero_cols] = self.X[self.nonzero_rows, self.nonzero_cols]
+        S = self.U @ self.V.T
+        S[self.nonzero_rows, self.nonzero_cols] = self.X[
+            self.nonzero_rows, self.nonzero_cols
+        ]
 
         return S
 
@@ -115,45 +137,54 @@ class RidgePenalty:
             self.oldV = np.copy(self.V)
             self.solve_inner()
             self.iteration += 1
-            if (np.linalg.norm(self.oldU@self.oldV.T - self.U@self.V.T) /
-                    np.linalg.norm(self.U@self.V.T) < self.tol):
-                print('Converged!')
+            if (
+                np.linalg.norm(self.oldU @ self.oldV.T - self.U @ self.V.T)
+                / np.linalg.norm(self.U @ self.V.T)
+                < self.tol
+            ):
+                print("Converged!")
                 return True
             return False
         self.solve_inner()
         self.iteration += 1
         return False
 
-    def calculate_loglikelihood(self, Xp, theta=2.5):
+    def calculate_loglikelihood(self, X_pred, theta_estimate=2.5):
+        """For each row y in X_pred, calculate the loglikelihood of y having originated
+        from the reconstructed continuous profile of all the patients in the training set.
         """
-        For each row y in Xp, calculate the loglikelihood of y having
-        originated from M_l for l = 1, ... N_1.
-        """
-        logposterior = lambda d: -theta * d**2
 
-        trainM = self.U@self.V.T
+        def loglikelihood_func(eta):
+            return -theta_estimate * eta ** 2
 
-        logL = np.ones((Xp.shape[0], trainM.shape[0]))
+        trainM = self.U @ self.V.T
 
-        for i in range(Xp.shape[0]):
-            row_nonzero_cols = Xp[i] != 0
-            inside = (Xp[i, row_nonzero_cols])[None, :] - trainM[:, row_nonzero_cols]
-            loglik_i = np.sum(logposterior(inside), axis=1)
-            logL[i] = loglik_i
+        N_1 = trainM.shape[0]
+        N_2 = X_pred.shape[0]
+
+        logL = np.ones((N_2, N_1))
+
+        for i in range(N_2):
+            row_nonzero_cols = X_pred[i] != 0
+            linear_predictors = (X_pred[i, row_nonzero_cols])[None, :] - trainM[
+                :, row_nonzero_cols
+            ]
+            logL[i] = np.sum(loglikelihood_func(linear_predictors), axis=1)
 
         return logL
 
-    def predict(self, Xp, t, output_domain=np.arange(1, 5), theta=2.5):
+    def predict(self, X_pred, t, output_domain=np.arange(1, 5), theta_estimate=2.5):
+        """For each row y in X_pred, calculate the most likely integer value for a future
+        fixed time t.
         """
-        For each row y in Xp, predict the most likely integer.
-        """
 
-        logL = self.calculate_loglikelihood(Xp)
+        def distribution_z(eta):
+            return np.exp(-theta_estimate * eta ** 2)
 
-        trainM = self.U@self.V.T
+        logL = self.calculate_loglikelihood(X_pred)
 
-        # Probability of sampling integer d_t at time t 
-        # for all risk profiles M_{i, :}, i = 1, ..., N_1 in the training data
-        prob_of_z_given_m = np.exp(-theta*(trainM[:, t, None] - output_domain)**2) 
+        trainM = self.U @ self.V.T
 
-        return np.exp(logL)@prob_of_z_given_m
+        p_of_z_given_m = distribution_z(trainM[:, t, None] - output_domain)
+
+        return np.exp(logL) @ p_of_z_given_m

619e3f1449fa715033f6d290200887ee4b3525ab
Mikal Stapnes <mikal.stapnes@ntnu.no>
2020-02-06 15:51:23 +0100

    Update f1, f2 with matrix input

--- models/matrix_factorization.py
+++ models/matrix_factorization.py
@@ -163,37 +163,29 @@ class MatrixFactorizationTesting(MatrixFactorization):
             + self.lambda3 * np.linalg.norm(self.C @ self.R @ V) ** 2
         )
 
-    def f1(self, u):
-        U = u.reshape((self.N, self.K))
+    def f1(self, U):
         return (
             self.lambda0 * np.linalg.norm(self.S - U @ self.V.T) ** 2
             + self.lambda1 * np.linalg.norm(U) ** 2
         )
 
-    def grad1(self, u):
-        U = u.reshape((self.N, self.K))
+    def grad1(self, U):
         return (
             -2 * self.lambda0 *
             (self.S - U @ self.V.T) @ self.V + 2 * self.lambda1 * U
         ).reshape(self.N * self.K)
 
-    def f2(self, v):
-        V = v.reshape((self.T, self.K))
-
+    def f2(self, V):
         res1 = self.lambda0 * np.linalg.norm(self.S - self.U @ V.T) ** 2
         res2 = self.lambda2 * np.linalg.norm(V - self.J) ** 2
         res3 = self.lambda3 * np.linalg.norm((self.C @ self.R) @ V) ** 2
 
         return res1 + res2 + res3
 
-    def grad2(self, v):
-        V = v.reshape((self.T, self.K))
-
+    def grad2(self, V):
         res1 = -2 * self.lambda0 * (self.S.T - V @ self.U.T) @ self.U
         res2 = 2 * self.lambda2 * (V - self.J)
-
-        CR = self.C @ self.R
-        res3 = 2 * self.lambda3 * (CR).T @ ((CR) @ V)
+        res3 = 2 * self.lambda3 * self.RTCTCR @ V
 
         return (res1 + res2 + res3).reshape(self.T * self.K)
 

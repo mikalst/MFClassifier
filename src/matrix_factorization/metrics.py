9cc99713cc51fefaed45cb3b1fa0d876e913137e
Mikal Stapnes <mikal.stapnes@ntnu.no>
2020-03-06 16:14:59 +0100

    Fix logical errors and renaming

--- src/matrix_factorization/metrics.py
+++ src/matrix_factorization/metrics.py
@@ -1,20 +1,21 @@
 import numpy as np
 import sklearn
+import copy
 import tqdm.autonotebook as tqdm
 
 
-def evaluate(model, data_obj, output_result_obj, idx_output_array, X_reals_ground_truth=None):
+def evaluate(model, data_obj, output_result_obj, idx_output_result, X_reals_ground_truth=None):
 
     N_STEPS_BIAS = output_result_obj.attrs['N_STEPS_BIAS']
     N_Z = output_result_obj.attrs['N_Z']
 
     if not(X_reals_ground_truth is None):
-        output_result_obj['recMSE'][idx_output_array] = np.mean(
+        output_result_obj['recMSE'][idx_output_result] = np.mean(
             ((X_reals_ground_truth[data_obj.train_idc] -
               model.U@(model.V.T))[data_obj.X_train == 0])**2
         )
 
-    output_result_obj['predSSE'][idx_output_array] = np.mean(
+    output_result_obj['predSSE'][idx_output_result] = np.mean(
         ((data_obj.X_train - model.U@model.V.T)[data_obj.X_train != 0])**2
     )
 
@@ -35,8 +36,8 @@ def evaluate(model, data_obj, output_result_obj, idx_output_array, X_reals_groun
         )
 
         cm = sklearn.metrics.confusion_matrix(data_obj.y_pred > 1, predicted_e)
-        output_result_obj['sensitivity_with_bias'][N_STEPS_BIAS*idx_output_array + i_bias] = cm[1, 1] / np.sum(cm[1, :])
-        output_result_obj['specificity_with_bias'][N_STEPS_BIAS*idx_output_array + i_bias] = cm[0, 0] / np.sum(cm[0, :])
+        output_result_obj['sensitivity_with_bias'][N_STEPS_BIAS*idx_output_result + i_bias] = cm[1, 1] / np.sum(cm[1, :])
+        output_result_obj['specificity_with_bias'][N_STEPS_BIAS*idx_output_result + i_bias] = cm[0, 0] / np.sum(cm[0, :])
 
     # Classification full
     predicted_integers = model.predict(
@@ -45,24 +46,29 @@ def evaluate(model, data_obj, output_result_obj, idx_output_array, X_reals_groun
         p_z_precomputed=posterior_probability
     )
 
-    output_result_obj['cms'][idx_output_array:idx_output_array+N_Z**2] = (sklearn.metrics.confusion_matrix(
-        data_obj.y_pred, predicted_integers)).flatten()
+    output_result_obj['cms'][(N_Z**2)*idx_output_result:(N_Z**2)*idx_output_result+(N_Z**2)] = (sklearn.metrics.confusion_matrix(
+        data_obj.y_pred, predicted_integers, labels=range(1, N_Z+1))).flatten()
 
 
 def evaluate_all_folds(model, data_obj, output_result_obj, idx_output_result, X_reals_ground_truth=None, verbose=False):
 
-    N_FOLDS = data_obj.attrs['N_FOLDS']
+    N_FOLDS = data_obj.n_splits
 
     if verbose:
-        pbar = tqdm.tqdm(total=data_obj.n_splits)
+        pbar = tqdm.tqdm(total=N_FOLDS)
 
-    for i_fold in range(data_obj.n_splits):
+    for i_fold in range(N_FOLDS):
+
+        print('Selecting fold {}'.format(i_fold))
 
         data_obj.select_fold(i_fold)
         model.Y = data_obj.X_train
+        model.reset()
         model.train()
 
         evaluate(model, data_obj, output_result_obj, N_FOLDS*idx_output_result+i_fold, X_reals_ground_truth=X_reals_ground_truth)
+
+        print(np.array(output_result_obj['recMSE']))
 
         if verbose:
             pbar.update()

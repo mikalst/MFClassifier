4728ec82e2e9ef9ae89e0d9d8524eae1d66445cd
Mikal Stapnes <mikal.stapnes@ntnu.no>
2020-03-07 15:11:02 +0100

    Cleanup

--- src/matrix_factorization/data.py
+++ src/matrix_factorization/data.py
@@ -3,15 +3,18 @@ import sklearn.model_selection
 
 
 class TemporalData:
-    def __init__(self, data):
+    def __init__(self, data, ground_truth=None):
         self.X = np.array(data)
         self.N = self.X.shape[0]
         self.T = self.X.shape[1]
 
+        if not(ground_truth is None):
+            self.ground_truth = ground_truth
+
 
 class TemporalDataPrediction(TemporalData):
-    def __init__(self, data, prediction_rule='last_observed', prediction_window=4):
-        super(TemporalDataPrediction, self).__init__(data)
+    def __init__(self, data, ground_truth=None, prediction_rule='last_observed', prediction_window=4):
+        super(TemporalDataPrediction, self).__init__(data, ground_truth)
 
         self.prediction_rule = prediction_rule
         self.prediction_window = 4
@@ -48,35 +51,59 @@ class TemporalDataPrediction(TemporalData):
 
 
 class TemporalDataKFold(TemporalData):
-    def __init__(self, data, prediction_rule, prediction_window=4, n_splits=5):
-        super(TemporalDataKFold, self).__init__(data)
+    def __init__(self, data, ground_truth=None, prediction_rule='last_observed', prediction_window=4, n_splits=5):
+
+        if not(ground_truth is None):
+            self.ground_truth = ground_truth
 
         self.prediction_rule = prediction_rule
         self.prediction_window = prediction_window
         self.n_splits = n_splits
 
-        self.pred_obj = TemporalDataPrediction(data=self.X, prediction_rule=self.prediction_rule, prediction_window=self.prediction_window)
-        self.train_obj = TemporalData(data=self.X[self.pred_obj.valid_rows])
+        self.__pred_obj = TemporalDataPrediction(data=data, prediction_rule=self.prediction_rule, prediction_window=self.prediction_window)
+        self.__train_obj = TemporalData(data=data[self.__pred_obj.valid_rows])
 
         kf = sklearn.model_selection.KFold(n_splits, shuffle=False)
-        self.fold_indices = [idc for idc in kf.split(self.train_obj.X)]
-        self.i_fold = 0
-        self.train_idc, self.pred_idc = self.fold_indices[self.i_fold]
+        self.__idc_per_fold = [idc_fold for idc_fold in kf.split(self.__train_obj.X)]
+        
+        #Instantiate with 1st fold
+        self.__i_fold = 0
+        self.__idc_train, self.__idc_pred = self.__idc_per_fold[self.i_fold]
+        
+    @property
+    def X_train(self):
+        return self.__train_obj.X[self.__idc_train]
 
-        self.X_train = self.train_obj.X[self.train_idc]
-        self.X_pred_regressor = self.pred_obj.X[self.pred_idc]
-        self.y_pred = self.pred_obj.y[self.pred_idc]
-        self.time_of_prediction = self.pred_obj.time_of_prediction[self.pred_idc]
+    @property
+    def X_pred_regressor(self):
+        return self.__pred_obj.X[self.__idc_pred]
 
-    def select_fold(self, i_fold):
-        assert(i_fold < self.n_splits)
-        self.i_fold = i_fold
-        self.train_idc, self.pred_idc = self.fold_indices[i_fold]
+    @property
+    def y_pred(self):
+        return self.__pred_obj.y[self.__idc_pred]
 
-        self.X_train = self.train_obj.X[self.train_idc]
-        self.X_pred_regressor = self.pred_obj.X[self.pred_idc]
-        self.y_pred = self.pred_obj.y[self.pred_idc]
-        self.time_of_prediction = self.pred_obj.time_of_prediction[self.pred_idc]
+    @property
+    def time_of_prediction(self):
+        return self.__pred_obj.time_of_prediction[self.__idc_pred]
 
-    def get_fold_idc(self):
-        return self.fold_indices[self.i_fold]
+    @property
+    def ground_truth_train(self):
+        if (self.ground_truth is None):
+            return None 
+        return self.ground_truth[self.__idc_train]
+
+    @property
+    def ground_truth_pred(self):
+        if (self.ground_truth is None):
+            return None 
+        return self.ground_truth[self.__idc_pred]
+
+    @property
+    def i_fold(self):
+        return self.__i_fold
+
+    @i_fold.setter
+    def i_fold(self, i_fold):
+        assert(int(i_fold) < self.n_splits)
+        self.__i_fold = int(i_fold)
+        self.__idc_train, self.__idc_pred = self.__idc_per_fold[self.__i_fold]

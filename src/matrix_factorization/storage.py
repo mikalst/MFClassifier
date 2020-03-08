32fa2370d22003940e93668a67ef1f3ba7173fb0
Mikal Stapnes <mikal.stapnes@ntnu.no>
2020-03-08 14:04:28 +0100

    Move save function to now be a member method

--- src/matrix_factorization/storage.py
+++ src/matrix_factorization/storage.py
@@ -2,6 +2,7 @@ import numpy as np
 from multiprocessing import Array
 import h5py
 
+
 class Result(dict):
     def __init__(self, N_STEPS_L1, N_STEPS_L3, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE=False):
 
@@ -22,56 +23,56 @@ class Result(dict):
             'compute_recMSE': compute_recMSE
         }
 
+    def save(self, path, identifier):
+        with h5py.File(
+        path+identifier, "w"
+        ) as outfile:
+        
+            shape = (
+                self.attrs['N_STEPS_L1'],
+                self.attrs['N_STEPS_L3'],
+                self.attrs['N_FOLDS']
+            )
+
+            outfile.create_dataset(
+                "cms",
+                data=np.array(self["cms"]).reshape(
+                    shape+(self.attrs['N_Z'], self.attrs['N_Z'])
+                )
+            )
+
+            outfile.create_dataset(
+                "sensitivity_with_bias",
+                data=np.array(self["sensitivity_with_bias"]).reshape(
+                    shape+(self.attrs['N_STEPS_BIAS'], )
+                )
+            )
+
+            outfile.create_dataset(
+                "specificity_with_bias",
+                data=np.array(self["specificity_with_bias"]).reshape(
+                    shape+(self.attrs['N_STEPS_BIAS'], )
+                )
+            )
+
+            outfile.create_dataset(
+                "predSSE",
+                data=np.array(self["predSSE"]).reshape(shape)
+            )
+
+            if self.attrs['compute_recMSE']:
+                outfile.create_dataset(
+                    "recMSE",
+                    data=np.array(self["recMSE"]).reshape(shape)
+                )
+            
+            for key in self.attrs.keys():
+                outfile.attrs[key] = self.attrs[key]
+
+
 class SharedMemoryResult(Result):
     def __init__(self, N_STEPS_L1, N_STEPS_L3, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE=False):
         super(SharedMemoryResult, self).__init__(N_STEPS_L1, N_STEPS_L3, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE)
 
         for key in self.keys():
             self[key] = Array('d', (self[key]))
-
-
-def store_results(result_obj, path_to_storage, identifier):
-    with h5py.File(
-        path_to_storage+identifier, "w"
-    ) as outfile:
-        
-        shape = (
-            result_obj.attrs['N_STEPS_L1'],
-            result_obj.attrs['N_STEPS_L3'],
-            result_obj.attrs['N_FOLDS']
-        )
-
-        outfile.create_dataset(
-            "cms",
-            data=np.array(result_obj["cms"]).reshape(
-                shape+(result_obj.attrs['N_Z'], result_obj.attrs['N_Z'])
-            )
-        )
-
-        outfile.create_dataset(
-            "sensitivity_with_bias",
-            data=np.array(result_obj["sensitivity_with_bias"]).reshape(
-                shape+(result_obj.attrs['N_STEPS_BIAS'], )
-            )
-        )
-
-        outfile.create_dataset(
-            "specificity_with_bias",
-            data=np.array(result_obj["specificity_with_bias"]).reshape(
-                shape+(result_obj.attrs['N_STEPS_BIAS'], )
-            )
-        )
-
-        outfile.create_dataset(
-            "predSSE",
-            data=np.array(result_obj["predSSE"]).reshape(shape)
-        )
-
-        if result_obj.attrs['compute_recMSE']:
-            outfile.create_dataset(
-                "recMSE",
-                data=np.array(result_obj["recMSE"]).reshape(shape)
-            )
-        
-        for key in result_obj.attrs.keys():
-            outfile.attrs[key] = result_obj.attrs[key]

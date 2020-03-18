36af80faa95aa34251c70ababa930303a918cd7c
Mikal Stapnes <mikal.stapnes@ntnu.no>
2020-03-18 14:30:01 +0100

    Fix exhaustive search tuning

--- dgdpredict/model_selection.py
+++ dgdpredict/model_selection.py
@@ -3,12 +3,14 @@ import multiprocessing
 from multiprocessing import Array
 import h5py
 
+import sklearn.metrics
 
+
+N_STEPS_BIAS = 100
 
 
 class Result(dict):
-    def __init__(self, N_SEARCH_POINTS, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE=False):
-
+    def __init__(self, N_SEARCH_POINTS, N_FOLDS, N_Z, compute_recMSE=False):
         # Input parameters
         self['lambda0'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS))
         self['lambda1'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS))
@@ -18,10 +20,11 @@ class Result(dict):
         self['theta'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS))
 
         # Scoring measures
-        self['cms'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS*N_Z**2))
+        self['confusion_matrix'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS*N_Z**2))
+        self['accuracy_bin_with_bias'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS*N_STEPS_BIAS))
         self['sensitivity_with_bias'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS*N_STEPS_BIAS))
         self['specificity_with_bias'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS*N_STEPS_BIAS))
-        self['predSSE'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS))
+        self['SSE'] = np.empty(shape=(N_SEARCH_POINTS*N_FOLDS))
 
         # Optional scoring measure
         if compute_recMSE:
@@ -82,38 +85,79 @@ class Result(dict):
 
 
 class SharedMemoryResult(Result):
-    def __init__(self, N_SEARCH_POINTS, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE=False):
-        super(SharedMemoryResult, self).__init__(N_SEARCH_POINTS, N_FOLDS, N_Z, N_STEPS_BIAS, compute_recMSE)
+    def __init__(self, N_SEARCH_POINTS, N_FOLDS, N_Z, compute_recMSE=False):
+        super(SharedMemoryResult, self).__init__(N_SEARCH_POINTS, N_FOLDS, N_Z, compute_recMSE)
 
         for key in self.keys():
             self[key] = Array('d', (self[key]))
+
+
+
+def _score(model, data_obj, result_obj, idx):
+
+    result_obj['lambda0'][idx] = model.lambda0
+    result_obj['lambda1'][idx] = model.lambda1
+    result_obj['lambda2'][idx] = model.lambda2
+    result_obj['lambda3'][idx] = model.lambda3
+    result_obj['K'][idx] = model.K
+    result_obj['theta'][idx] = model.theta
+
+    N_Z = result_obj.attrs['N_Z']
+    predicted_z = model.predict(data_obj.X_pred_regressor, data_obj.time_of_prediction)
+    result_obj['confusion_matrix'][idx*N_Z**2: (idx+1)*N_Z**2] = (
+        sklearn.metrics.confusion_matrix(
+            data_obj.y_true,
+            predicted_z,
+            labels=model.domain_z
+        )
+    ).flatten()
+
+    predicted_proba_binary = model.predict_proba_binary(data_obj.X_pred_regressor, data_obj.time_of_prediction)
+    y_true_bin = model.z_to_binary_mapping(data_obj.y_true)
+    for i_bias, bias in enumerate(np.linspace(0, 1, N_STEPS_BIAS)):
+        predict_bin = predicted_proba_binary >= bias
+        result_obj['sensitivity_with_bias'][N_STEPS_BIAS*idx + i_bias] = np.mean(predict_bin[y_true_bin == 1] == y_true_bin[y_true_bin == 1])
+        result_obj['specificity_with_bias'][N_STEPS_BIAS*idx + i_bias] = np.mean(predict_bin[y_true_bin == 0] == y_true_bin[y_true_bin == 0])
+        result_obj['accuracy_bin_with_bias'][N_STEPS_BIAS*idx + i_bias] = np.mean(predict_bin == y_true_bin)
+
+    result_obj['SSE'][idx] = np.mean(((model.U@model.V.T - model.X_train)[model.nonzero_rows, model.nonzero_cols])**2)
+    
+    # Optional scoring measure
+    if result_obj.attrs['compute_recMSE']:
+        result_obj['recMSE'][idx] = np.mean(((model.U@model.V.T - data_obj.ground_truth_train)[~model.nonzero_rows, ~model.nonzero_cols])**2)
 
 
 def search(
     data_obj,
     model_generator,
     idc_parameter_select,
-    results
+    result_obj
 ):
+
+    N_FOLDS = result_obj.attrs['N_FOLDS']
 
     for idx in idc_parameter_select:
     
         model = model_generator(idx)
-        model.fit(data_obj.X_train)
-        model.score(data_obj, results, idx)
 
-        print("Running idx:", idx)
+        for i_fold in range(N_FOLDS):
+    
+            data_obj.i_fold = i_fold
+            model.fit(data_obj.X_train)
+            _score(model, data_obj, result_obj, N_FOLDS*idx + i_fold)
 
 
 def search_parallelize(
     data_obj,
     model_generator,
     idc_parameter_select,
-    results,
+    result_obj,
     N_CPU
 ):
 
     idc_per_cpu = np.array_split(idc_parameter_select, N_CPU)
+
+    print(idc_per_cpu)
 
     workers = []
     for i_cpu in range(N_CPU):
@@ -122,7 +166,7 @@ def search_parallelize(
                     data_obj,
                     model_generator,
                     idc_per_cpu[i_cpu],
-                    results
+                    result_obj
                 )
             )
         )
